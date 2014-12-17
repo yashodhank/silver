2c423eddb5b5eb1cf5b4cd4b50f1d1e217a72829
bogdanpetrea <bogdan@presslabs.com>
2014-12-17 10:59:03 +0200

    Added the Swagger '/docs/' url.

--- silver/urls.py
+++ silver/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'', include('silver.api.urls', namespace='silver_api'))
 )
